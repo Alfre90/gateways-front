98be7fe415bfcd7f5aacb80c283725531ce60ddc
Alfre Martinez <alfrermtmadrid@gmail.com>
2022-10-05 19:11:03 -0400

    Added tests

--- src/app/features/devices/services/devices.service.spec.ts
+++ src/app/features/devices/services/devices.service.spec.ts
@@ -1,17 +1,46 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { IAddGateway } from '@features/gateways/interfaces/add-gateway';
+import { GatewaysService } from '@features/gateways/services/gateways.service';
+import { IAddDevice } from '../interfaces/add-device';
 
 import { DevicesService } from './devices.service';
 
 describe('DevicesService', () => {
   let service: DevicesService;
+  let gatewayService: GatewaysService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({ imports: [HttpClientModule] });
     service = TestBed.inject(DevicesService);
+    gatewayService = TestBed.inject(GatewaysService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create a device', (done: DoneFn) => {
+    let device: IAddDevice = {
+      gatewayId: 1,
+      vendor: 'Apple',
+      status: false,
+      created: '2022-10-05'
+    };
+    let gateway: IAddGateway = {
+      name: 'Apple',
+      serialNumber: (
+        Math.floor(Math.random() * (100000 - 0 + 1)) + 0
+      ).toString(),
+      iPv4: '111.111.111.1'
+    };
+    gatewayService.addGateway(gateway).subscribe(() => {
+      service.addDevice(device).subscribe(() => {
+        service.getAll('', 'GatewayId==1', 1, 10).subscribe((data) => {
+          expect(data.rowCount).toBeGreaterThanOrEqual(1);
+          done();
+        });
+      });
+    });
   });
 });
--- src/app/features/gateways/services/gateways.service.spec.ts
+++ src/app/features/gateways/services/gateways.service.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { AppStoreModule } from '@store/store.module';
+import { IAddGateway } from '../interfaces/add-gateway';
 
 import { GatewaysService } from './gateways.service';
 
@@ -16,5 +17,57 @@ describe('GatewaysService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create a device', (done: DoneFn) => {
+    let gateway: IAddGateway = {
+      name: 'Apple',
+      serialNumber: (
+        Math.floor(Math.random() * (100000 - 0 + 1)) + 0
+      ).toString(),
+      iPv4: '111.111.111.1'
+    };
+    service.addGateway(gateway).subscribe(() => {
+      service.getAll('', '', 1, 10).subscribe((data) => {
+        expect(data.rowCount).toBeGreaterThanOrEqual(1);
+        done();
+      });
+    });
+  });
+
+  it('should get a gateway', (done: DoneFn) => {
+    let gateway: IAddGateway = {
+      name: 'Apple',
+      serialNumber: (
+        Math.floor(Math.random() * (100000 - 0 + 1)) + 0
+      ).toString(),
+      iPv4: '111.111.111.1'
+    };
+    service.addGateway(gateway).subscribe(({ id }) => {
+      service.getById(id).subscribe((data) => {
+        expect(data.id).toBe(id);
+        done();
+      });
+    });
+  });
+
+  it('should remove a gateway', (done: DoneFn) => {
+    let gateway: IAddGateway = {
+      name: 'Apple',
+      serialNumber: (
+        Math.floor(Math.random() * (100000 - 0 + 1)) + 0
+      ).toString(),
+      iPv4: '111.111.111.1'
+    };
+    service.getAll('', '', 1, 10).subscribe((data) => {
+      service.addGateway(gateway).subscribe(({ id }) => {
+        service.deleteGateway(id).subscribe(() => {
+          service.getAll('', '', 1, 10).subscribe((data2) => {
+            expect(data.rowCount).toBe(data2.rowCount);
+            done();
+          });
+        });
+      });
+    });
   });
 });
